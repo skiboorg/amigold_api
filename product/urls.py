996e8b123701e19eeb619a10d6926b7510d4d069
xxx <xxx@xxx.xxx>
2023-08-15 11:08:04 +0300

    1

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('types', views.GetTypes.as_view()),
     path('all', views.GetProducts.as_view()),
     path('<slug>', views.GetProduct.as_view()),
-    path('types', views.GetTypes.as_view()),
+
 ]
